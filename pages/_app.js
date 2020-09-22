f4bee7b44dbae704dac594247a17b30a299de823
Jack Gisel <jackrgisel@gmail.com>
2020-09-22 11:22:55 -0700

    working on styling the blog

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import "../styles/index.css";
 function MyApp({ Component, pageProps }) {
   return (
     <Container pageTitle="Jack Gisel" description="Jack Gisel's Blog">
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </Container>
   );
 }
